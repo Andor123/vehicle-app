fb4169e160e96aa054361fd03faed5fda9dbf3ae
Andor Salamon <salamon.andor@gmail.com>
2019-09-05 08:39:55 +0200

    Add Startup.cs for Razor Pages config Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+public class Startup 
+{
+    public void ConfigureServices(IServiceCollection services) 
+    {
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app) 
+    {
+        app.UseMvc();
+    }
+}

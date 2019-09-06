cdfd070cfc78d002ad7dc5801bc91336176b5587
Andor Salamon <salamon.andor@gmail.com>
2019-09-06 08:44:51 +0200

    Database creation Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

---
+++ vehicle-app.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE vehicleapp;
+CREATE TABLE vehicles 
+(
+    VehicleId INTEGER,
+    RegistrationNumber VARCHAR(45),
+    PrivateCode VARCHAR(45),
+    TypeOfVehicle VARCHAR(45),
+    DateStamp DATETIME
+)

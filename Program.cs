42f8f867090c46a4280d28d17a0f187884f0e9a8
Andor Salamon <salamon.andor@gmail.com>
2019-09-06 09:01:06 +0200

    Update Program.cs Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace vehicle_app2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return RedirectToPage("/Index");
         }
     }
 }

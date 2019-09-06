cab4154b64ccd5cd9235f50f3b019dbf10d4c773
Andor Salamon <salamon.andor@gmail.com>
2019-09-06 08:10:52 +0200

    Update Startup and add database file Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,13 @@ public class Startup
     public void ConfigureServices(IServiceCollection services) 
     {
         services.AddMvc();
+        services.AddEntityFramework()
+        .AddSqlite()
+        .AddDbContext<TodoDbContext>();
+
+        services.AddIdentity<ToDoUser, IdentityRole>()
+        .AddEntityFrameworkStores<TodoDbContext>()
+        .AddDefaultTokenProviders();
     }
 
     public void Configure(IApplicationBuilder app) 

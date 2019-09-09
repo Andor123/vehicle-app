dd5ec3edd066c8ed034ac162f43e69ef6141ce4f
Andor Salamon <salamon.andor@gmail.com>
2019-09-09 09:11:05 +0200

    Add Vehicles model Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

---
+++ Vehicles.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesContacts.Data 
+{
+    public class Vehicles 
+    {
+        public int VehicleId { get; set; }
+
+        [Required, StringLength(45)]
+        public string RegistrationNumber { get; set; }
+
+        [Required, StringLength(45)]
+        public string PrivateCode { get; set; }
+
+        [Required, StringLength(45)]
+        public string TypeOfVehicle { get; set; }
+
+        public DateTime DateStamp { get; set; }
+    }
+}

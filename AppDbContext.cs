f5406a60db38a597b31ddefb7e84fb8c1fc87ce4
Andor Salamon <salamon.andor@gmail.com>
2019-09-10 15:15:55 +0200

    Update project Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -7,5 +7,10 @@ namespace RazorPagesContacts.Data
         public AppDbContext(DbContextOptions options) : base(options) {}
 
         public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlite(@"Data Source=IdentitySample.sqlite;");
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,28 +6,23 @@ using RazorPagesContacts.Data;
 
 namespace RazorPagesContacts 
 {
-public class Startup 
-{
-    public void ConfigureServices(IServiceCollection services) 
+    public class Startup 
     {
-        services.AddMvc();
-        services.AddEntityFramework()
-        .AddSqlite()
-        .AddDbContext<TodoDbContext>();
-
-        services.AddIdentity<ToDoUser, IdentityRole>()
-        .AddEntityFrameworkStores<TodoDbContext>()
-        .AddDefaultTokenProviders();
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddMvc();
+            services.AddEntityFramework()
+            .AddSqlite()
+            .AddDbContext<TodoDbContext>();
+            
+            services.AddIdentity<ToDoUser, IdentityRole>()
+            .AddEntityFrameworkStores<TodoDbContext>()
+            .AddDefaultTokenProviders();
+        }
+        
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.UseMvc();
+        }
     }
-
-    public void Configure(IApplicationBuilder app) 
-    {
-        app.UseMvc();
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-    {
-        optionsBuilder.UseSqlite(@"Data Source=IdentitySample.sqlite;");
-    }
-}
 }

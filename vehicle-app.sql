03461bd9912f63f28e4abc489640c23dd1b61abe
Andor Salamon <salamon.andor@gmail.com>
2019-09-10 09:06:42 +0200

    Add datatables Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

--- vehicle-app.sql
+++ vehicle-app.sql
@@ -1,9 +1,1 @@
-CREATE DATABASE vehicleapp;
-CREATE TABLE vehicles 
-(
-    VehicleId INTEGER,
-    RegistrationNumber VARCHAR(45),
-    PrivateCode VARCHAR(45),
-    TypeOfVehicle VARCHAR(45),
-    DateStamp DATETIME
-);
+SELECT * FROM USERS;
